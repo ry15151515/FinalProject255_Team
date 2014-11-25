4f1fc09efa47d76598b7bd34db205328a2160ab1
Leandro Vicente Silva <lleandrovicente@gmail.com>
2014-11-25 15:59:27 -0500

    included missing classes combo box item and cls maintenance

---
+++ FinalProject_Team/FinalProject_Team/clsComboBoxItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_Team
+{
+    public class clsComboBoxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+        string test;
+
+        public override string ToString() //Override is need because Text has a new behavior on this class. Right?
+        {
+            return Text;
+        }
+    }
+}
---
+++ FinalProject_Team/FinalProject_Team/clsMaintenance.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject_Team
+{
+
+    class clsMaintenance
+    {
+        string printer;
+        string test;
+        public string Printer
+        {
+            get { return printer; }
+            set { printer = value; }
+        }
+        DateTime dateTime;
+
+        public DateTime DateTime
+        {
+            get { return dateTime; }
+            set { dateTime = value; }
+        }
+        string cateogory;
+
+        public string Cateogory
+        {
+            get { return cateogory; }
+            set { cateogory = value; }
+        }
+        string details;
+
+        public string Details
+        {
+            get { return details; }
+            set { details = value; }
+        }
+
+        public clsMaintenance()
+        {
+            printer = "";
+            dateTime = DateTime.Now;
+            cateogory = "";
+            details = "";
+        }
+
+        public clsMaintenance(string myPrinter, DateTime myDateTime, string myCategory, string myDetails)
+        {
+            printer = myPrinter;
+            dateTime = myDateTime;
+            cateogory = myCategory;
+            details = myDetails;
+        }
+
+
+    }
+}
